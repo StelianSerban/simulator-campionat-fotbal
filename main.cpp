984a0a84bef47b3f211e03cc630d2b43659cb534
StelianSerban <stelianserban17@gmail.com>
2025-05-07 16:44:19 +0000

    Added abstract class Persoana and class Jucator

--- main.cpp
+++ main.cpp
@@ -1,48 +1,91 @@
 #include <iostream>
 #include <array>
+#include <string>
+using namespace std;
+
+class Persoana 
+{
+
+protected:
+    string nume;
+    int varsta;
+
+public:
+    Persoana() : nume(""), varsta(0) {}
+    Persoana(string nume, int varsta) : nume(nume), varsta(varsta) {}
+    Persoana(Persoana &other) : nume(other.nume), varsta(other.varsta) {}
+
+    Persoana &operator=(Persoana other)
+    {
+        nume = other.nume;
+        varsta = other.varsta;
+
+        return this;
+    }
+
+    virtual ~Persoana() {}
+
+    string getNume()
+    {
+        return nume;
+    }
+    int getVarsta()
+    {
+        return varsta;
+    }
+
+    virtual void afisare() const = 0;
+};
+
+class Jucator : public Persoana
+{
+
+private:
+    string pozitie;
+    int numar;
+    
+public:
+    Jucator() : Persoana(), pozitie("N/A"), numar(0) {}
+    Jucator(const string nume, int varsta, string pozitie, int numar) : Persoana(nume, varsta), pozitie(pozitie), numar(numar) {}
+    Jucator(Jucator &other) : Persoana(other), pozitie(other.pozitie), numar(other.numar) {}
+
+    ~Jucator() {}
+
+    Jucator &operator=(Jucator other)
+    {
+        Persoana::operator=(other);
+        pozitie = other.pozitie;
+        numar = other.numar;
+
+        return this;
+    }
+
+    bool operator==(Jucator other)
+    {
+        return nume == other.nume && numar == other.numar
+    }
+
+    string getPozitie()
+    {
+        return pozitie;
+    }
+    int getNumar()
+    {
+        return numar;
+    }
+    void afisare() override 
+    {
+        cout<<"Nume: "<<nume<<\n<<"Pozitie: "<<pozitie<<\n<<"Numar: "<<numar<<\n<<"Varsta: "<<varsta<<\n;
+    }
+    friend ostream &operator<<(ostream &out, Jucator j)
+    {
+        out << j.nume << " " << j.pozitie << " " << j.numar << " " << j.varsta;
+        return out;
+    }
+};
+
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
+    
     return 0;
 }
